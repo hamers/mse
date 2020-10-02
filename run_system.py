104b792eb78915779da0e5e366da4f25ab473fed
Adrian Hamers <adrian.hamers@gmail.com>
2020-10-02 11:18:39 +0200

    Bugfix

--- run_system.py
+++ run_system.py
@@ -27,17 +27,17 @@ def parse_arguments():
 
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("--configuration",                  type=str,                   dest="configuration",               default="fully nested",             help="Configuration: `fully nested' or `2+2 quadruple'.")
-    parser.add_argument("--masses",                         type=float,  nargs="+",     dest="masses",                      default=[40.0,10.0,2.0],            help="Masses/MSun")
-    parser.add_argument("--metallicities",                  type=float,  nargs="+",     dest="metallicities",                      default=[0.02,0.02,0.02],           help="Metallicities")
-    parser.add_argument("--smas",                  type=float,  nargs="+",     dest="semimajor_axes",                      default=[15.0,120.0],           help="Semimajor axes (au)")
-    parser.add_argument("--es",                  type=float,  nargs="+",     dest="eccentricities",                      default=[0.1,0.1],           help="Eccentricities")
-    parser.add_argument("--is",                  type=float,  nargs="+",     dest="inclinations",                      default=[0.0001,85.0*np.pi/180.0],           help="Inclinations (rad)")
-    parser.add_argument("--LANs",                  type=float,  nargs="+",     dest="longitudes_of_ascending_node",                      default=[0.01,0.01],           help="Longitudes of the ascending node (rad)")
-    parser.add_argument("--APs",                  type=float,  nargs="+",     dest="arguments_of_pericentre",                      default=[0.01,0.01],           help="Arguments of periapsis (rad)")
+    parser.add_argument("--configuration",                            type=str,                   dest="configuration",               default="fully_nested",             help="Configuration: `fully_nested' or `2+2_quadruple'.")
+    parser.add_argument("--masses", "--ms",                           type=float,  nargs="+",     dest="masses",                      default=[40.0,10.0,2.0],            help="Masses/MSun")
+    parser.add_argument("--metallicities", "--zs",                    type=float,  nargs="+",     dest="metallicities",                      default=[0.02,0.02,0.02],           help="Metallicities")
+    parser.add_argument("--smas", "--semimajor_axes",                 type=float,  nargs="+",     dest="semimajor_axes",                      default=[15.0,120.0],           help="Semimajor axes (au)")
+    parser.add_argument("--es",  "--eccentricities",                  type=float,  nargs="+",     dest="eccentricities",                      default=[0.1,0.1],           help="Eccentricities")
+    parser.add_argument("--is", "--inclinations",                     type=float,  nargs="+",     dest="inclinations",                      default=[0.0001,85.0*np.pi/180.0],           help="Inclinations (rad)")
+    parser.add_argument("--LANs", "--longitudes_of_ascending_node",   type=float,  nargs="+",     dest="longitudes_of_ascending_node",                      default=[0.01,0.01],           help="Longitudes of the ascending node (rad)")
+    parser.add_argument("--APs", "--arguments_of_pericentre",         type=float,  nargs="+",     dest="arguments_of_pericentre",                      default=[0.01,0.01],           help="Arguments of periapsis (rad)")
     
-    parser.add_argument("--tend",                  type=float,  dest="end_time",                      default=2.0e7,           help="Integration time (yr)")
-    parser.add_argument("--Nsteps",                  type=int,  dest="N_steps",                      default=2000,           help="Number of plot output steps")
+    parser.add_argument("--tend",                           type=float,  dest="end_time",                    default=2.0e7,           help="Integration time (yr)")
+    parser.add_argument("--Nsteps",                         type=int,   dest="N_steps",                      default=2000,           help="Number of plot output steps")
     
     parser.add_argument("--plot_filename",                  type=str,  dest="plot_filename",                      default="test1",           help="Plot filename")
     
@@ -66,5 +66,5 @@ if __name__ == '__main__':
     print("Integration time (yr): ",args.end_time)
     print("Number of plot output steps: ",args.N_steps)
     
-    stellar_types = [1,1,1]
+    stellar_types = [1 for x in range(N_bodies)]
     Tools.evolve_system(args.configuration,N_bodies,args.masses,args.metallicities,args.semimajor_axes,args.eccentricities,args.inclinations,args.arguments_of_pericentre,args.longitudes_of_ascending_node,args.end_time,args.N_steps,stellar_types=stellar_types,plot_filename=args.plot_filename)
